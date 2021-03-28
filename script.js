fc4ddbed52f411a815a29d42f47f07d1c983e1c6
Brian Huh <brianhuh01@gmail.com>
2021-03-28 02:32:57 -0400

    functional calculator

--- script.js
+++ script.js
@@ -1,29 +1,154 @@
 const numbers = document.querySelectorAll("button.num")
 const calcs = document.querySelectorAll("button.calc")
 const results = document.querySelectorAll("button.result")
-let first = 0
+const displayMain = document.querySelector(".displayMain")
+const displayFirst = document.querySelector(".displayFirst")
+const displaySecond = document.querySelector(".displaySecond")
+const tempDisplay = document.querySelector(".tempDisplay")
+const operand = document.querySelector(".operand")
 
-const display = document.querySelector(".display")
-let input = numbers.forEach((number) => 
-    number.addEventListener("click", function() {
-    display.innerText += this.innerText
-    first = display.innerText
-})) // by class naem?
-    //make two lines. one for display and one internal calculation
-
-
-console.log(display.innerText)
-
-calcs.forEach((calc) => 
-    calc.addEventListener("click", function() {
-    if (display.innerText.slice(-1) == "x" || display.innerText.slice(-1) == "%" 
-    || display.innerText.slice(-1) == "+" || display.innerText.slice(-1) == "-" ) {
+let first = ""
+let oper = ""
+let second = ""
 
 
-        input = `${input} ${this.innerText}`}
+
+numbers.forEach((number) => {
+     
+        number.addEventListener("click", function() {           
+
+
+            
+            if (oper === '') {
+                displayFirst.innerText += this.innerText
+                first = displayFirst.innerText 
+                calc()
+                console.log("number first OR oper ===''")
+            }
+    
+            else if (oper !== '') {
+                displayFirst.innerText = ''
+                displaySecond.innerText += this.innerText
+                second = displaySecond.innerText //make two lines. one for displayFirst and one internal calculation
+                console.log("number second OR oper !== ''")
+
+            }
+        
+        
+        
+        })})
+    
+
+/////////// equal button then makes the answer to displayFirst and erase displayFirst 1 and displayFirst 2 and operand
+
+function calc() {
+    calcs.forEach((calc) => 
+        calc.addEventListener("click", function() {
+            if (oper !== "" && first != '' && second != '') {
+                console.log("calc() oper =='' and displaySeonc.innertext !== ''")
+                console.log(tempDisplay.innerText)
+                operate()
+                tempDisplay.innerText = displayFirst.innerText
+
+            }
+            
+            else if (oper === "") {
+                console.log("calc() oper===''")        
+                tempDisplay.innerText = displayFirst.innerText
+            }
+
+            
+                oper = this.innerText
+                operand.innerText = this.innerText
+        //maybe second function here ?
+       
+
     }))
+}
+
+results.forEach((result) =>
+    result.addEventListener("click", operate)) //
 
 
-numbers.forEach((number) => 
-    number.addEventListener("click", e => {
-    console.log(e)}))
+
+function operate() {
+    if (oper === '+') {
+        displayFirst.innerText = addition(first, second)
+    } else if (oper === '-') {
+        displayFirst.innerText = subtraction(first, second)
+    } else if (oper === '%') {
+        displayFirst.innerText = division(first, second)
+    } else if (oper === 'x') {
+        displayFirst.innerText = multip(first, second)
+    }
+    console.log("operate() outside the ifs")
+    first = displayFirst.innerText
+    displaySecond.innerText = ""
+    tempDisplay.innerText = ""
+    operand.innerText = ""
+    oper = ""
+    second = ''
+}
+
+
+function addition(a, b) {
+    return parseInt(a)+parseInt(b)
+}
+
+function subtraction(a, b) {
+    return parseInt(a)-parseInt(b)
+}
+
+function division(a, b) {
+    if (b == 0) {
+        return ("cannot divide by 0")
+    }
+    return parseInt(a)/parseInt(b)
+}
+
+function multip(a, b) {
+    return parseInt(a)*parseInt(b)
+}
+
+
+
+
+
+
+
+
+// numbers.forEach((number) => {
+//     if (oper === '') {
+//         number.addEventListener("click", function() {
+//         displayFirst.innerText += this.innerText
+//         first = displayFirst.innerText 
+//         calc()
+//         console.log('first')
+//             })}
+    
+//     else if (oper !== '') {
+//         number.addEventListener("click", function() {
+//         displayFirstSecond.innerText += this.innerText
+//         second = displayFirstSecond.innerText //make two lines. one for displayFirst and one internal calculation
+//         console.log("second")
+//         switch(oper) {
+//             case '+':
+//                 addition()
+//                 break;
+//             case '-':
+//                 subtraction()
+//                 break;
+//             case '%':
+//                 division()
+//                 break;
+//             case 'x':
+//                 multip()
+//                 break;
+//         }
+// })}
+
+//     }
+    
+    
+//     )
+    
